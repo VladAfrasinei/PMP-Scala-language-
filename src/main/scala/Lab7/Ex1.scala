5d3a52f6bc8bc0648cd0aec8020f212294e7418b
VladAfrasinei <45604842+VladAfrasinei@users.noreply.github.com>
2020-11-12 12:05:54 +0200

    lab7

--- src/main/scala/Lab7/Ex1.scala
+++ src/main/scala/Lab7/Ex1.scala
@@ -2,14 +2,20 @@ package Lab7
 
 import com.cra.figaro.language._
 import com.cra.figaro.algorithm.sampling._
+import com.cra.figaro.library.atomic.continous.Uniform
+import com.cra.figaro.library.atomic.sampling.Importante
 
 object Ex1 {
 	def main(args: Array[String]) {
-		val test = Constant("Test")
+		val s=Uniform(0,0.7)
+		val holes=Map(1->2,2->3,3->4,4->5,5->6,6->7,7->8,8->9,9->10,10->11,11->12,12->13,13->14,14->15,
+		15->16,16->17,17->18)
+		for(hole <- holes)
+		{
+			pass(holes).observe(hole)
 
-		val algorithm = Importance(1000, test)
-		algorithm.start()
-		
-		println(algorithm.probability(test, "Test"))
+		}
+		val ve =
+ VariableElimination(holes)
 	}
 }
